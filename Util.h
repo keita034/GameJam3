7a38c016b15538d69ae0ca210a58743c530405a2
keita_momiyama <93181795+keita034@users.noreply.github.com>
2023-09-05 08:35:51 +0900

    ファイル関数追加

--- Util.h
+++ Util.h
@@ -4,18 +4,4 @@
 #include<Windows.h>
 #include<filesystem>
 
-std::vector<std::string> getFileNames(const std::string& folderPath_)
-{
-	std::filesystem::directory_iterator lDirectoryIterator(folderPath_), lIteratorEnd;
-	std::error_code lErrorCode;
-	std::vector<std::string> lFileNames;
-
-	for (; lDirectoryIterator != lIteratorEnd && !lErrorCode; lDirectoryIterator.increment(lErrorCode))
-	{
-		const std::filesystem::directory_entry lEntry = *lDirectoryIterator;
-
-		lFileNames.push_back(lEntry.path().string());
-	}
-
-	return lFileNames;
-}
+std::vector<std::string> GetFileNames(const std::string& folderPath_);
